864e7acf06e732ec3101040f395f14013cec2cf4
Said Oudouane <saidoudouane@gmail.com>
2022-02-15 22:52:17 +0100

    modify users table to add unique columns and add posts table

---
+++ development/database/migrations/2022_02_15_212844_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("poster");
+            $table->string("trailer");
+            $table->string("banner");
+            $table->text("description");
+            $table->date("release_date");
+            $table->integer("period");
+            $table->foreignId("author_id")->constrained("users")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
