ff47deb9c2bd20de2f63bf7832846db5fe115b92
Said Oudouane <saidoudouane@gmail.com>
2022-02-26 18:58:24 +0100

    add Post and create posts table migration

--- dev/app/models/Post.php
+++ dev/app/models/Post.php
@@ -4,5 +4,8 @@ namespace App\Models;
 
 class Post extends Model
 {
-
+    protected string $table = "posts";
+    protected $required = [
+        "title", "author_id", "poster", "banner",
+    ];
 }
