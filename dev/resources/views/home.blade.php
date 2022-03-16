399edcb75334258cd110cb57b3608c63400621e2
Said Oudouane <saidoudouane@gmail.com>
2022-03-16 17:59:15 +0100

    change class diagram to reflect new system

--- dev/resources/views/home.blade.php
+++ dev/resources/views/home.blade.php
@@ -1,15 +1,11 @@
 @component("layouts.layout")
     @styles("pages/home")
     <div class="home">
-        {{--        intro with featured posts --}}
         @auth
             @if(!($isMyPosts ?? false))
                 <a class="home__posts" href="/user/posts">my posts</a>
             @endif
         @endauth
         @include("components.posts")
-        {{--        @if(count($posts) > 0)--}}
-        {{--            <a class="home__more" href="/posts">view more</a>--}}
-        {{--        @endif--}}
     </div>
 @endcomponent
